4659c4ebbac90402026a5ba19508f25337999c50
Tetsuwo181 <hamakaze181and189amarube@gmail.com>
2018-03-22 21:27:37 +0900

    Add files via upload

--- module/algos/select/test/conftest.py
+++ module/algos/select/test/conftest.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 from ....indi import individual as INDI
 from ....indi import population as POP
+from ....test_problem import test_problem as TP
+
 
 """
 テストに使う個体を初期化
 テスト問題は遺伝子中の１の数をカウント
 """
-incubator = INDI.incubator_builder(TP.binary_num, True, False)
+incubator = INDI.incubator_builder(TP.binary_num, True)
 test_indi_bin1 = incubator([0, 0, 0, 0, 0])
 test_indi_bin2 = incubator([1, 0, 0, 0, 0])
 test_indi_bin3 = incubator([1, 1, 0, 0, 0])
@@ -14,6 +16,6 @@ test_indi_bin4 = incubator([1, 0, 0, 1, 0])
 test_indi_bin5 = incubator([1, 1, 1, 1, 0])
 test_indi_bin6 = incubator([1, 1, 1, 1, 1])
 
-test_population_bin = POP.Population([test_indi1, test_indi2, test_indi3, test_indi4, test_indi5])
+test_population_bin = POP.Population([test_indi_bin1, test_indi_bin2, test_indi_bin3, test_indi_bin4, test_indi_bin5])
 
 
